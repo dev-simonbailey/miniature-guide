8989a5a579cd2e4d151004e4225c2e34d19612bc
dev-simonbailey <30531868+dev-simonbailey@users.noreply.github.com>
2021-03-25 16:51:02 +0000

    Cleanup in head blade

--- resources/views/global/head.blade.php
+++ resources/views/global/head.blade.php
@@ -1,50 +1,40 @@
 <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Bluesky</title>
-
-        <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
-
-        <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
 
-    <!-- Styles -->
-<style>
-    html, body {
-        background-color: #fff;
-        color: #636b6f;
-        font-family: 'Nunito', sans-serif;
-        font-weight: 200;
-        height: 100vh;
-        margin: 0;
-    }
+            .content {
+                text-align: center;
+            }
 
-    .content {
-        text-align: center;
-    }
+            .title {
+                font-size: 84px;
+            }
 
-    .title {
-        font-size: 84px;
-    }
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
 
-    .links > a {
-        color: #636b6f;
-        padding: 0 25px;
-        font-size: 13px;
-        font-weight: 600;
-        letter-spacing: .1rem;
-        text-decoration: none;
-        text-transform: uppercase;
-    }
-
-    .m-b-md {
-        margin-bottom: 30px;
-    }
-</style>
-
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
     </head>
