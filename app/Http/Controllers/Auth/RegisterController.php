14b7e8c9bf07683431d0baa54d5ced7e2a53f2f2
dev-simonbailey <30531868+dev-simonbailey@users.noreply.github.com>
2021-03-30 10:49:10 +0100

    Altered login to accomodate additional fields

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,9 +49,11 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'username'      => ['required', 'string', 'max:255'],
+            'name'          => ['required', 'string', 'max:255'],
+            'department'    => ['required', 'string', 'max:255'],
+            'email'         => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password'      => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
 
@@ -64,9 +66,13 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'username'      => $data['username'],
+            'name'          => $data['name'],
+            'department'    => $data['department'],
+            'email'         => $data['email'],
+            'role'          => 'default',
+            'home'          => '/',
+            'password'      => Hash::make($data['password']),
         ]);
     }
 }
