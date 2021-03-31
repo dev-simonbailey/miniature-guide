a7991d4ac7776a40813b1806f82f74e89e82ce3d
dev-simonbailey <30531868+dev-simonbailey@users.noreply.github.com>
2021-03-31 12:51:47 +0100

    tidied up the code

--- resources/views/reports/bike_wip_stats.blade.php
+++ resources/views/reports/bike_wip_stats.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
+
 @section('page_title')
     {{ config('app,name')}} - {{ "Bike WIP Stats" }}
 @endsection
+
 @section('content')
     <div class="content">
         <div class="title m-b-md">
